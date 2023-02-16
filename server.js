74e280e16843f569c727d7310aee4b2e561edd08
Neider Urbano <jjuly8julianur@gmail.com>
2023-02-16 15:10:02 -0500

    resolved activity on server done in express

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-//servidor con modulo HTTP
-// const http = require("http");
-// const port = 3000;
-// const host = "127.0.0.1";
-// const data = require("./data.json");
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { "Content-Type": "application/json" });
-//   res.write(
-//     JSON.stringify({
-//       task: data,
-//     })
-//   );
-//   res.end();
-// });
-
-// server.listen(port, host, () => {
-//   console.log("listening on port " + port, host);
-// });
-
 //servidor con EXPRESS
 const express = require("express");
 const app = express();
