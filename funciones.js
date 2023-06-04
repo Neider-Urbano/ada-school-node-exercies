94f728ae6d94061592aaa730148553d1a34a60c1
Neider Urbano <jjuly8julianur@gmail.com>
2023-06-03 22:01:38 -0500

    lista de tareas asincrona en node

--- funciones.js
+++ funciones.js
@@ -1,45 +1,63 @@
-function Task(){
-    this.list=new Array({
-        "id": 1,
-        "description": "ver TV",
-        "state": false
-    },{
-        "id": 2,
-        "description": "comer hamburguesa",
-        "state": false
+function Task() {
+  this.list = new Array(
+    {
+      id: 1,
+      description: "ver TV",
+      state: false,
+    },
+    {
+      id: 2,
+      description: "comer hamburguesa",
+      state: false,
     }
-    );
+  );
 }
 
-Task.prototype.add=function (data){
-    var id=null;
-    if(this.list.length>0) id=Math.max(...this.list?.map(task => task.id))+1
-    else id=1
-    const json={
-        "id": id,
-        "description": data,
-        "state": false
+Task.prototype.add = function (data) {
+  var id = null;
+  if (this.list.length > 0)
+    id = Math.max(...this.list?.map((task) => task.id)) + 1;
+  else id = 1;
+  const json = {
+    id: id,
+    description: data,
+    state: false,
+  };
+
+  this.list.push(json);
+  let taskData = this.list;
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(taskData);
+    }, 3000);
+  });
+};
+
+Task.prototype.deletes = function (id) {
+  const newArray = this.list?.filter((task) => task.id != id);
+  this.list = newArray;
+  let taskData = this.list;
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(taskData);
+    }, 3000);
+  });
+};
+
+Task.prototype.complete = function (id) {
+  const newArray = this.list?.map((task) => {
+    if (task.id == id) {
+      task.state = !task.state;
     }
+    return task;
+  });
+  this.list = newArray;
+  let taskData = this.list;
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(taskData);
+    }, 3000);
+  });
+};
 
-    this.list.push(json)
-    return this.list
-}
-
-Task.prototype.deletes=function(id){
-    const newArray=this.list?.filter((task)=>task.id!=id)
-    this.list=newArray
-    return this.list
-}
-
-Task.prototype.complete=function(id){
-    const newArray=this.list?.map((task)=>{
-        if(task.id==id){
-            task.state=!task.state
-        }
-        return task;
-    }) 
-    this.list=newArray
-    return this.list
-}
-
-module.exports = { Task }
+module.exports = { Task };
--- index.js
+++ index.js
@@ -1,79 +1,115 @@
-const {Task}= require("./funciones.js")
+const { Task } = require("./funciones.js");
 
-const myTasks=new Task()
+const myTasks = new Task();
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-
-readline.question(`
+readline.question(
+  `
   ---Bienvenido:---
   1. Agregar
   2. Delete
   3. Completar
   4. Exit
   Elige una opcion: 
-`, num => {
-    if(num==1){
-      readline.question(`
+`,
+  (num) => {
+    if (num == 1) {
+      readline.question(
+        `
         description:  
-      `, description => {
-          console.log(myTasks.add(description))
-      });
+      `,
+        async (description) => {
+          console.log(await myTasks.add(description));
+        }
+      );
     }
-    if(num==2){
-      readline.question(`
+    if (num == 2) {
+      readline.question(
+        `
         id tasks delete:  
-      `, idTask => {
-          console.log(myTasks.deletes(idTask))
-      });
+      `,
+        async (idTask) => {
+          console.log(await myTasks.deletes(idTask));
+        }
+      );
     }
-    if(num==3){
-      readline.question(`
+    if (num == 3) {
+      readline.question(
+        `
         id tasks complete:  
-      `, idTask => {
-          console.log(myTasks.complete(idTask))
-      });
+      `,
+        async (idTask) => {
+          console.log(await myTasks.complete(idTask));
+        }
+      );
     }
-    if(num==4){
+    if (num == 4) {
       readline.close();
     }
-});
 
-readline.on('line',(input)=>{
-  readline.question(`
+    if (num > 4) {
+      console.log("Invalid number");
+    }
+  }
+);
+
+readline.on("line", (input) => {
+  readline.question(
+    `
   ---Bienvenido:---
   1. Agregar
   2. Delete
   3. Completar
   4. Exit
-  Elige una opcion: 
-`, num => {
-    if(num==1){
-      readline.question(`
-        description:  
-      `, description => {
-          console.log(myTasks.add(description))
-      });
+  Elige una opcion:
+`,
+    (num) => {
+      if (num == 1) {
+        readline.question(
+          `
+        description:
+      `,
+          (description) => {
+            myTasks.add(description).then((data) => {
+              console.log(data);
+            });
+          }
+        );
+      }
+      if (num == 2) {
+        readline.question(
+          `
+        id tasks delete:
+      `,
+          (idTask) => {
+            myTasks.deletes(idTask).then((data) => {
+              console.log(data);
+            });
+          }
+        );
+      }
+      if (num == 3) {
+        readline.question(
+          `
+        id tasks complete:
+      `,
+          (idTask) => {
+            myTasks.complete(idTask).then((data) => {
+              console.log(data);
+            });
+          }
+        );
+      }
+      if (num == 4) {
+        readline.close();
+      }
+      if (num > 4) {
+        console.log("Invalid number");
+      }
     }
-    if(num==2){
-      readline.question(`
-        id tasks delete:  
-      `, idTask => {
-          console.log(myTasks.deletes(idTask))
-      });
-    }
-    if(num==3){
-      readline.question(`
-        id tasks complete:  
-      `, idTask => {
-          console.log(myTasks.complete(idTask))
-      });
-    }
-    if(num==4){
-      readline.close();
-    }
+  );
 });
-})
